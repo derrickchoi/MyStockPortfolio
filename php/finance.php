96625f60e44c428f4adec16dda976cd30dd99f6f
Jingyu Wang <wangjingyu0422@gmail.com>
2016-02-28 22:07:42 -0800

    new finance.php

--- php/finance.php
+++ php/finance.php
@@ -1,66 +1,73 @@
 <?php
+    
+    class financeAPI {
+        private $data;
+        
+    
+        public function getData($tickers) {
+        
+            // set url
+            $url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20%28%22%27'.implode(',',$tickers).'%27%22%29&env=store://datatables.org/alltableswithkeys';
+        
+            
 
-/*
- fetch stock data from Yahoo Finance API
-*/
+            $options = array('Symbol','Name','Open','PreviousClose','PercentChange',
+                                'LastTradePriceOnly');
+            
+            // request data
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+            $resp = curl_exec($ch);
+            $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            curl_close($ch);
+            
+            //parse data
+            if (!empty($options)) {
+                $xml = new SimpleXMLElement($resp);
+                $data = array();
+                $col = array();
+                if (is_object($xml)){
+                    foreach($xml->results->quote as $quote){
+                        $col=array();
+                        foreach($options as $option){
+                            $col[$option] = (string) $quote->$option;
+                        }
+                        $data[$col['Symbol']] = $col;
+                    }
+                } else{
+                    $data = $resp;
+                }
+                // store it private data member
+                $this->data = $data;
+            }
+        }
+        
+        public function getCurrentPrice($ticker){
+            return $this->data[$ticker]['LastTradePriceOnly'];
+        }
+        
+        public function getPrevClosingPrice($ticker){
+            return $this->data[$ticker]['PreviousClose'];
+        }
+        
+        public function getOpeningPrice($ticker){
+            return $this->data[$ticker]['Open'];
+        }
+        
+        public function getPercentChanged($ticker){
+            return $this->data[$ticker]['PercentChange'];
+        }
+        
+        public function getCompanyName($ticker){
+            echo $this->data[$ticker]['Name'];
+        }
+        
+    
+    }
+    
+
     
 
 
-
-function getCurrentPrice($ticker){
-    // return current price of the stock
-    
-    // fetch data from Yahoo API
-    // parameter l1 = Last Trade(Price Only)
-    $currentPrice = file_get_contents("http://finance.yahoo.com/d/quotes/csv?s=".$ticker."&f=l1&e=.csv");
-
-    return $currentPrice;
-    
-}
-
-function getCompanyName($ticker){
-    //return company name of the stock
-    
-    // fetch data from Yahoo API
-    // parameter n = Name
-    $name = file_get_contents("http://finance.yahoo.com/d/quotes/csv?s=".$ticker."&f=n&e=.csv");
-    
-    // removing the quotation marks
-    $name = str_replace('"', "", $name);
-    return $name;
-}
-    
-function getClosingPrice($ticker){
-    //return previous closing price of the stock
-    
-    // fetch data from Yahoo API
-    // parameter p = Prev Close(The closing price for the trading day prior to the last trade reported)
-    $prevClose = file_get_contents("http://finance.yahoo.com/d/quotes/csv?s=".$ticker."&f=p&e=.csv");
-    return $prevClose;
-}
-    
-function getOpeningPrice($ticker){
-    //return opening price of the stock
-    
-    // fetch data from Yahoo API
-    // parameter o = Open
-    $open = file_get_contents("http://finance.yahoo.com/d/quotes/csv?s=".$ticker."&f=o&e=.csv");
-    return $open;
-}
-    
-function getPercentChanged($ticker){
-    //return percent change of the stock
-    
-    // fetch data from Yahoo API
-    // parameter p2 = Change in Percent
-    $change = file_get_contents("http://finance.yahoo.com/d/quotes/csv?s=".$ticker."&f=p2&e=.csv");
-    $change = str_replace('"', "", $change);
-
-    return $change;
-
-}
-
-
-    
 
 ?>
