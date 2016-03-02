8810032140deaa37ee90f53d3fab4dd958756710
Utkash Dubey <udubey@usc.edu>
2016-03-02 00:08:33 -0800

    Added buying stock functionality, for now the price for any stock is hardcoded and other finance checks need to be added

---
+++ php/trade_form_handle.php
@@ -0,0 +1,64 @@
+<html>
+<?php
+
+// require_once("finance0.php");
+require_once("db_query.php");
+
+if ($_GET['confirmed'] == "true") {
+	// user has confirmed that they want to buy and we should execute the purchase
+	$email = $_GET['email'];
+	$ticker = $_GET['ticker'];
+	$amount = $_GET['amount'];
+	buy($email, $ticker, $amount, /*getCurrentPrice($ticker)*/50.50);
+	echo '<body onload="returnToDashboard()"><script type="text/javascript">
+			function returnToDashboard() {
+				window.location.href = "..";
+			}
+			</script>
+			';
+} else if (isset($_POST['buy_button'])) { // user clicked the button to buy and just got to this form handler
+
+	$ticker = $_POST['ticker'];
+	$companyName = $_POST['companyName'];
+	$amount = $_POST['amount'];
+	$email = $_GET['email'];
+
+	if ($ticker == "") {
+		$ticker = getTicker($companyName);
+	}
+
+	// first verify that the buy action is valid
+	if (false/*!marketIsOpen() || $ticker == -1 || !isValidTicker($ticker) || $amount * getCurrentPrice($ticker) > getBalance($email)*/) {
+		echo '<body>Invalid trade';
+	} else {
+		// the buy action is valid
+		// confirm the purchase with the user
+		echo '<body onload="confirmPurchase()">
+				<script type="text/javascript">
+					function confirmPurchase() {
+						var confirmed = confirm("Are you sure you want to purchase ' . $amount . ' shares of ' . $ticker . '?");
+						if (confirmed) {
+							window.location.href = "trade_form_handle.php?email=' . $email . 'confirmed=true&ticker=' . $ticker . '&amount=' . $amount . '";
+						} else {
+							window.location.href = "..";
+						}
+					} </script>';
+		// execute the purchase -- happens in line 4 ( if (isset($_GET['confirmed'])) {...} )
+	}
+
+} else if (isset($_POST['sell_button'])) { // user clicked the button to sell
+
+} else {
+	// user got here without submitting the form, and we redirect them back
+	echo '<body onload="returnToDashboard()">
+			<script type="text/javascript">
+				function returnToDashboard() {
+					window.location.href = "..";
+				}
+			</script>
+			';
+}
+
+?>
+</body>
+</html>
