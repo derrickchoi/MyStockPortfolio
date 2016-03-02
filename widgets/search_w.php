c4b59b7a713d07675209a8274166d3c4ecf36eba
Derrick Choi <derriccc@usc.edu>
2016-03-02 01:30:48 -0800

    search updated

--- widgets/search_w.php
+++ widgets/search_w.php
@@ -1,14 +1,7 @@
 <?php
 
-	$ticker = array(
-		"appl" => "apple",
-		"fb" => "facebook",
-		"goog" => "google",
-	);
-
 ?>
 <br />
-<form action="php/search_auto.php">
 	<div class="form-inline">
 		<div class="form-group">
 
@@ -19,8 +12,8 @@
 		    <link rel="stylesheet" href="/resources/demos/style.css">
 			<script src="search_auto.js"></script>
 
-			<label for="stock">Stock: </label>
-			<input type="text" class="form-control" id="stock" placeholder="Search for a stock">
+			<label for="stock">Stock Search: </label>
+			<input type="text" class="form-control" id="searchStock" placeholder="Search for a stock">
 			<script>
 			  jQuery(document).ready(function($){
 			    var availableStock = [
@@ -43,38 +36,77 @@
 			      "Intel",
 			      "INTC"
 			    ];
-			    $( "#stock" ).autocomplete({
+			    $( "#searchStock" ).autocomplete({
 			      source: availableStock
 			    });
 			  });
 			</script>
 
+			<script>
+			document.getElementById("searchStock").addEventListener("keypress", function(event) {
+				if (event.keyCode === 13) {
+					var table = document.getElementById("stock-list");
+					var row = table.appendChild(document.createElement("tr"));
+
+					var name = row.appendChild(document.createElement("td"));
+					name.textContent = this.value;
+
+					var display = row.appendChild(document.createElement("td"));
+					display.textContent = "display from";
+
+					var startDate = row.appendChild(document.createElement("td"));
+					startDate.contentEditable = true;
+					startDate.textContent = "9/29";
+
+					var to = row.appendChild(document.createElement("td"));
+					to.textContent = "to";
+
+					var endDate = row.appendChild(document.createElement("td"));
+					endDate.contentEditable = true;
+					endDate.textContent = "2/29";
+
+					var graphRow = row.appendChild(document.createElement("td"));
+
+					var graphButton = graphRow.appendChild(document.createElement("button"));
+					graphButton.classList.add("btn", "btn-default");
+					graphButton.textContent = "Graph";
+
+					var watchlistRow = row.appendChild(document.createElement("td"));
+
+					var watchlistButton = watchlistRow.appendChild(document.createElement("button"));
+					watchlistButton.classList.add("btn", "btn-default");
+					watchlistButton.setAttribute("id", "watchlistButton");
+					watchlistButton.textContent = "Add to Watchlist";
+				}
+			});
+			</script>
+
+			<script>
+			startDate.addEventListener(“input”, function(event) {
+				console.log(this.textContent);
+			});
+
+			endDate.addEventListener(“input”, function(event) {
+				console.log(this.textContent);
+			});
+			</script>
+
+			<script>
+			// document.getElementById("searchStock").addEventListener("keypress", function(event) {
+			// 	onclick="myfunction()"
+
+			// function myfunction() {
+			// 	window.location.href = ".?ticker=" + encodeURI(ticker) + "?start=" + encodeURI(start date) + "?end=" + encodeURI(end date);
+			// }
+			</script>
+
 
 		</div>
 
-		<table class="table table-bordered">
- 			<tr>
- 				<td>Facebook Inc.</td>
- 				<td>display from</td>
- 				<td contentEditable>9/29</td>
- 				<td>to</td>
- 				<td contentEditable>2/29</td>
- 				<td <button class="btn btn-default">Graph</button>
- 				<td <button class="btn btn-default">Add to Watchlist</button>
- 			</tr>
+		<table class="table table-bordered" id="stock-list">
 
- 			<tr>
- 				<td>Fac & Co.</td>
- 				<td>display from</td>
- 				<td contentEditable>9/29</td>
- 				<td>to</td>
- 				<td contentEditable>2/29</td>
- 				<td <button class="btn btn-default">Graph</button>
- 				<td <button class="btn btn-default">Add to Wishlist</button>
- 			</tr>
 		</table>
 
 	</div>
 
-</form>
 <br />
