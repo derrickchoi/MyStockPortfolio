817c31db080c10b4aee535ade28297de7ab7438c
Mark Gauderman <Mark Gauderman>
2016-03-01 22:13:33 -0800

    beginnings of portfolio_w.php

--- widgets/portfolio_w.php
+++ widgets/portfolio_w.php
@@ -1,10 +1,19 @@
 <?php
 
-$portfolio = getPortfolio($email);
+$portfolio = getPortfolio($email); // gets the user's portfolio stocks
 
 echo "<br />Your portfolio<br />";
+echo "<table class=\"table table-bordered\">";
 foreach($portfolio as $ticker => $amount) {
+	echo "<tr>";
+		echo "<td>" . $ticker . "</td>";
+		echo "<td>" . tickername . "</td>";
+		echo "<td>" . $amount . "</td>";
+		echo "<td>" . currentPrice . "</td>";
+		echo "<td>" . checkbox . "</td>";
+	echo "<tr>";
 	echo $ticker . ": " . $amount . "<br />";
 }
+echo "</table>"; 
 
 ?>
