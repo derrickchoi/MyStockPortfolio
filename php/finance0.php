3f93062906abfdb6aae51d1c5d517087c17aba04
Jingyu Wang <wangjingyu0422@gmail.com>
2016-03-02 00:00:12 -0800

    commenting finance0.php

--- php/finance0.php
+++ php/finance0.php
@@ -7,9 +7,9 @@
     
 
 
-
+// return current price of the stock
+// sample: getCurrentPrice("goog");
 function getCurrentPrice($ticker){
-    // return current price of the stock
     
     // fetch data from Yahoo API
     // parameter l1 = Last Trade(Price Only)
@@ -19,8 +19,10 @@ function getCurrentPrice($ticker){
     
 }
 
+
+// return company name of the stock
+// sample: getCompanyName("goog");
 function getCompanyName($ticker){
-    //return company name of the stock
     
     // fetch data from Yahoo API
     // parameter n = Name
@@ -30,16 +32,21 @@ function getCompanyName($ticker){
     $name = str_replace('"', "", $name);
     return $name;
 }
-    
+
+
+//return previous closing price of the stock
+// sample:  getClosingPrice("goog");
 function getClosingPrice($ticker){
-    //return previous closing price of the stock
     
     // fetch data from Yahoo API
     // parameter p = Prev Close(The closing price for the trading day prior to the last trade reported)
     $prevClose = file_get_contents("http://finance.yahoo.com/d/quotes/csv?s=$ticker&f=p&e=.csv");
     return $prevClose;
 }
-    
+
+
+// return opening price of the stock
+// sample:  getOpeningPrice("goog");
 function getOpeningPrice($ticker){
     //return opening price of the stock
     
@@ -48,9 +55,11 @@ function getOpeningPrice($ticker){
     $open = file_get_contents("http://finance.yahoo.com/d/quotes/csv?s=$ticker&f=o&e=.csv");
     return $open;
 }
-    
+
+
+// return percent change of the stock
+// getPercentChanged("goog")
 function getPercentChanged($ticker){
-    //return percent change of the stock
     
     // fetch data from Yahoo API
     // parameter p2 = Change in Percent
@@ -61,6 +70,10 @@ function getPercentChanged($ticker){
 
 }
 
+
+// parameter for $startDate and $endDate "mmddyyyy"
+// returns the url to get csv file
+// samle: getHistoricalData('GOOG', "010102015", "01012016");
 function getHistoricalData($ticker, $startDate, $endDate){
     // pasing date
     $startMonth = substr($startDate, 0 , 2);
@@ -73,18 +86,19 @@ function getHistoricalData($ticker, $startDate, $endDate){
 
     $url = "http://ichart.finance.yahoo.com/table.csv?s=$ticker&a=$startMonth&b=$startDay&c=$startYear&d=$endMonth&e=$endDay&f=$endYear&g=d&ignore=.csv";
 
-    echo file_get_contents($url);
+    //file_put_contents("a.csv", file_get_contents($url));
 
     return $url;
 
 }
 
-function getIntradayData($ticker, $days){
-    $url = "http://chartapi.finance.yahoo.com/instrument/1.0/.$ticker./chartdata;type=quote;range=.$days.d/csv";
+// function getIntradayData($ticker, $days){
+//     $url = "http://chartapi.finance.yahoo.com/instrument/1.0/.$ticker./chartdata;type=quote;range=.$days.d/csv";
+// }
 
 
-}
-
+// returns true if ticker is valid. Otherwise false
+// sample: isValidTicker("goog");
 function isValidTicker($ticker){
     //$url = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query=$ticker&region=1&lang=en&callback=YAHOO.Finance.SymbolSuggest.ssCallback";
     $name = file_get_contents("http://finance.yahoo.com/d/quotes/csv?s=$ticker&f=n&e=.csv");
@@ -96,8 +110,10 @@ function isValidTicker($ticker){
 
 }
 
-function marketIsOpen(){
 
+// returns true if market is open. Otherwise false.
+// sample: marketIsOpen();
+function marketIsOpen(){
 
    date_default_timezone_set('US/Eastern');
    $currenttime = date('h:i:s:u');
@@ -111,9 +127,11 @@ function marketIsOpen(){
 
 }
 
-    echo getCurrentPrice("goog");
-    echo getClosingPrice("goog");
-    echo getPercentChanged("goog");
+    //echo getCurrentPrice("goog");
+    //echo getClosingPrice("goog");
+    //echo getPercentChanged("goog");
+    //echo getOpeningPrice("goog");
+    //echo getCompanyName("goog");
 
     //getHistoricalData('GOOG', "010102015", "01012016");
     //var_dump($a);
