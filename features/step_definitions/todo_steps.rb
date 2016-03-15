a27b968dd603edc2ddc9dbd5c010743470229acf
Utkash Dubey <udubey@usc.edu>
2016-03-15 06:30:03 -0700

    testing setup

--- features/step_definitions/todo_steps.rb
+++ features/step_definitions/todo_steps.rb
@@ -1,19 +1,15 @@
-Given(/^I am on the home page$/) do
-	pending #write code here
+Given /^I am on the home page$/ do
+    visit "http://localhost/mystockportfolio/index.php"
 end
-
-When(/^I click on New Item$/) do
-	pending
+When /^I click on New Item$/ do
+      pending # express the regexp above with the code you wish you had
 end
-
-When(/^I fill in the item$/) do
-	pending
+When /^I fill in the item$/ do
+      pending # express the regexp above with the code you wish you had
 end
-
-When(/^I click the button Submit$/) do
-	pending
+When /^I click the button Submit$/ do
+      pending # express the regexp above with the code you wish you had
 end
-
-Then(/^I should see the new item added to the list$/) do
-	pending
+Then /^I should see the new item added to the list$/ do
+      pending # express the regexp above with the code you wish you had
 end
