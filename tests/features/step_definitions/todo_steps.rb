cd386a4514e890e7e059748958ea6db07c39912f
Utkash Dubey <udubey@usc.edu>
2016-03-15 05:52:09 -0700

    see previous commit description

---
+++ tests/features/step_definitions/todo_steps.rb
@@ -0,0 +1,19 @@
+Given(/^I am on the home page$/) do
+	pending #write code here
+end
+
+When(/^I click on New Item$/) do
+	pending
+end
+
+When(/^I fill in the item$/) do
+	pending
+end
+
+When(/^I click the button Submit$/) do
+	pending
+end
+
+Then(/^I should see the new item added to the list$/) do
+	pending
+end
