b4654d4838884c70a8c8abce63e88bfd39a587e6
Utkash Dubey <udubey@usc.edu>
2016-03-15 06:46:06 -0700

    test setup contd

--- features/step_definitions/todo_steps.rb
+++ features/step_definitions/todo_steps.rb
@@ -1,15 +1,15 @@
 Given /^I am on the home page$/ do
-    visit "http://localhost/mystockportfolio/index.php"
+	visit "http://localhost/mystockportfolio/index.php"
 end
-When /^I click on New Item$/ do
-      pending # express the regexp above with the code you wish you had
+When /^I click on (.*)$/ do |link|
+    click_link (link)
 end
 When /^I fill in the item$/ do
-      pending # express the regexp above with the code you wish you had
+	pending # express the regexp above with the code you wish you had
 end
 When /^I click the button Submit$/ do
-      pending # express the regexp above with the code you wish you had
+	pending # express the regexp above with the code you wish you had
 end
 Then /^I should see the new item added to the list$/ do
-      pending # express the regexp above with the code you wish you had
+	pending # express the regexp above with the code you wish you had
 end
