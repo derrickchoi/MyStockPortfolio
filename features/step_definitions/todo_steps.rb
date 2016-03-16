9dd8c79afd4950dc43043585796ed67c2372e3dd
Utkash Dubey Linux <udubey@usc.edu>
2016-03-16 05:04:22 -0700

    Fixed sample test, looks good for blackbox testing

--- features/step_definitions/todo_steps.rb
+++ features/step_definitions/todo_steps.rb
@@ -2,10 +2,10 @@ Given /^I am on the login page$/ do
 	visit "http://localhost/mystockportfolio/index.php"
 end
 When /^I enter the email (.*)$/ do |email|
-	fill_in('Email address' with: email)
+	fill_in('Email address', with: email)
 end
 When /^I enter the password (.*)$/ do |password|
-	fill_in('Password' with: password)
+	fill_in('Password', with: password)
 end
 When /^I click on the button (.*)$/ do |link|
     click_button(link)
