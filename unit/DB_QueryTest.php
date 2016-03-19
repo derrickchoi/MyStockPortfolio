a40c3a925186feb43089baf62f732e617e66417d
Utkash Dubey <udubey@usc.edu>
2016-03-19 01:11:57 -0700

    testing phpunit test

---
+++ unit/DB_QueryTest.php
@@ -0,0 +1,12 @@
+<?php
+
+// require_once 'PHPUnit/Framework.php';
+require_once('../php/db_query.php');
+
+class DB_QueryTest extends PHPUnit_Framework_Testcase {
+
+	$this->assertEquals(true, true);
+
+}
+
+?>
