e03f52ccc9c95146b2469ec09b58d102edad45dc
Utkash Dubey Linux <udubey@usc.edu>
2016-03-19 01:32:41 -0700

    this has the start of db-query test, use this as a guide for php testing I guess

--- unit/DBQueryTest.php
+++ unit/DBQueryTest.php
@@ -1,11 +1,13 @@
 <?php
 
-// require_once 'PHPUnit/Framework.php';
+//require_once 'PHPUnit/Framework.php';
 require_once('../php/db_query.php');
 
 class DB_QueryTest extends PHPUnit_Framework_Testcase {
 
-	$this->assertEquals(true, true);
+	function test() {
+		$this->assertEquals(0, 0);
+	}
 
 }
 
