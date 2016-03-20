111a633a455f9b801dec582e22ed343215f288f4
Utkash Dubey <udubey@usc.edu>
2016-03-20 02:39:05 -0700

    pushing account management tests to test on VM

--- features/step_definitions/accountManagement_steps.rb
+++ features/step_definitions/accountManagement_steps.rb
@@ -7,9 +7,26 @@ end
 When /^I enter the password (.*)$/ do |password|
 	fill_in('Password', with: password)
 end
-When /^I click on the button (.*)$/ do |link|
-    click_button(link)
+When /^I click on the button (.*)$/ do |butt|
+    click_button(butt)
 end
 Then /^I should be forwarded to the Dashboard View$/ do
-	pending # express the regexp above with the code you wish you had
+	pending
+end
+
+Given /^I am on the dashboard page$/ do
+	if page.has_link?('Logout') do 
+		# do nothing, we're already on the dashboard
+	else
+		visit "http://localhost/mystockportfolio/index.php"
+		fill_in('Email address', with: 'udubey@usc.edu')
+		fill_in('Password', with: 'temporary')
+		click_button('Login')
+	end
+end
+When /^I click on the link (.*)$/ do |link|
+	click_link(link)
+end
+Then /^I should be forwarded to the Login View$/ do
+	pending
 end
