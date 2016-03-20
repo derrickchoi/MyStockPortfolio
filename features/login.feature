a1a5a2f685dbb497ebf2087586a5b6df480d00ad
Utkash Dubey Linux <udubey@usc.edu>
2016-03-20 04:01:55 -0700

    revamped login so it's specific to the login feature

---
+++ features/login.feature
@@ -0,0 +1,24 @@
+Feature: Login
+    In order to use the site
+    As a user
+    I need to be able to register, log in, and retrieve a forgotten password upon visiting the site
+    Scenario: Visiting the site
+        Given I have a clean session
+        When I visit the site
+        Then I should be forwarded to the Login View
+    Scenario: Logging in with valid credentials
+        Given I am on the login page
+        When I enter the email udubey@usc.edu
+        And I enter the password temporary
+        And I click on the button Login
+        Then I should be forwarded to the Dashboard View
+    Scenario: Logging in with invalid credentials
+        Given I am on the login page
+        When I enter the email udubey@usc.edu
+        And I enter the password foobar
+        And I click on the button Login
+        Then I should be forwarded to the Login View
+    Scenario: Logging out
+    	Given I am on the dashboard page
+    	When I click on the link Logout
+    	Then I should be forwarded to the Login View
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,5 +1,9 @@
 Given /^I am on the login page$/ do
-	visit "http://localhost/mystockportfolio/index.php"
+	if page.has_link?('Logout')
+		click_link('Logout')
+	else
+		visit "http://localhost/mystockportfolio/index.php"
+	end
 end
 When /^I enter the email (.*)$/ do |email|
 	fill_in('Email address', with: email)
@@ -11,12 +15,12 @@ When /^I click on the button (.*)$/ do |butt|
     click_button(butt)
 end
 Then /^I should be forwarded to the Dashboard View$/ do
-	pending
+	expect(page).to have_link('Logout')
 end
 
 Given /^I am on the dashboard page$/ do
+	visit 'http://localhost/mystockportfolio/index.php'
 	if !page.has_link?('Logout')
-		visit "http://localhost/mystockportfolio/index.php"
 		fill_in('Email address', with: 'udubey@usc.edu')
 		fill_in('Password', with: 'temporary')
 		click_button('Login')
@@ -26,5 +30,15 @@ When /^I click on the link (.*)$/ do |link|
 	click_link(link)
 end
 Then /^I should be forwarded to the Login View$/ do
-	pending
+	expect(page).to have_button('Login')
+end
+
+Given /^I have a clean session$/ do
+	# just to be extra sure that the session is reset
+	visit 'http://localhost'
+	Capybara.reset_sessions!
+	visit 'http://localhost/mystockportfolio/index.php'
+end
+When /^I visit the site$/ do
+	visit 'http://localhost/mystockportfolio/index.php'
 end
