fe2ef6535888f80f00d264c61a35c553009f28c2
Hailey Manuel <hmanuel@usc.edu>
2016-03-20 18:53:37 -0700

    search tests

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -1,9 +1,14 @@
 When /^I enter the ticker (.*)$/ do |ticker|
 	fill_in('searchStock', with: ticker)
 end
-Then /^I should see it auto filled with apple, aapl$/ do
-	expect(page).to have_content?("ui-id-3")
-	expect(page).to have_content?("ui-id-2")
+Then /^I should have the table stock-list$/ do
+	expect(page).to have_table 'stock-list'
+end
+And /^I should see the text (.*) on the table$/ do |stock|
+	expect(page).to have_css("stock-list", text: (stock))
+end
+And /^I should also see the text (.*) on the table$/ do |stock|
+	expect(page).to have_css("stock-list", text: (stock))
 end
 
 
